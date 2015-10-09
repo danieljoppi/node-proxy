c09c873592f9ab394fd81e87c1af9cb9b2f4b030
Daniel Henrique Joppi <daniel.joppi@gmail.com>
2015-10-09 17:12:07 -0300

    fix user-agent

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ function onRequest(client_req, client_res) {
        'Accept':'text/html',
         //'Accept-Encoding': 'gzip',
        'Accept-Language': 'en-US;q=0.6,en;q=0.4',
-       'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.12$
+       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.12'
     }
   };
 
